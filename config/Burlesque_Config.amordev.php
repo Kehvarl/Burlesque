91f9adda1ce16f03e8b7fc5d5c8319c17b9433c8
Code Dragon <Kehvarl@gmail.com>
2017-02-13 19:14:36 -0700

    Revert "Revert "Initial Commit""
    This reverts commit 597ddd06712d969bcb0ed42ea3c5bf51d4d55dae.

---
+++ config/Burlesque_Config.amordev.php
@@ -0,0 +1,8 @@
+<?php
+    $config['db']['host']             = "localhost";
+    $config['db']['port']             = "3306";
+    $config['db']['database']         = "amordev_burlesque";
+    $config['db']['username']         = "amordev_burlesque";
+    $config['db']['password']         = "i0hQAEL1hbPw";
+    $config['db']['table_prefix']     = "";
+?>
