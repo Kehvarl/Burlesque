b3dd52a667c98a77010f9917299f265b69003b6f
Kehvarl <Kehvarl@gmail.com>
2018-02-13 18:41:46 -0700

    Modify Burlesque module to use new Xenforo Integrations class instead of calling Xenforo functions directly

--- Burlesque.php
+++ Burlesque.php
@@ -1,36 +1,33 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/../config/Burlesque_Config.php');
 require_once('includes/xf_integrations.php');
-require_once('includes/classes.php');	
+require_once('includes/classes.php');
 require_once('includes/database.php');
 require_once('includes/Burlesque_BBCode.php');
 require_once('includes/Burlesque_Commands.php');
-		
+
 class Burlesque
 {
-    private $Visitor;
-    private $Session;
     private $InputData;
 
     private $Database;
-    
+
     private $user_color;
     private $user_font;
-    
+
     public $Output;
-    
+
     function __construct($config, $input_data)
     {
         //Connect to Xenforo
-        $this->Visitor = initialize($_SERVER['DOCUMENT_ROOT']);
-        $this->Session = XenForo_Session::startPublicSession();
-        
+				$this->XF = new Burlesque_Xenforo_Integration($_SERVER['DOCUMENT_ROOT']);
+
         //Setup Datetime module
-        $this->DT = new DateTime("now", 
-            new DateTimeZone($this->Visitor->get('timezone'))); 
+        $this->DT = new DateTime("now",
+            new DateTimeZone($this->XF->getVisitorTimezone());
         $timestamp = time();
         $this->DT->setTimestamp($timestamp);
-        
+
         //Connect to Database
         $this->Database = new Burlesque_DB_Tools(
                                      $config['db']['username'],
@@ -43,15 +40,15 @@ class Burlesque
         {
             error_log($this->Database->error);
         }
-        
+
         //Get information from user
         $this->InputData = $input_data;
     }
-    
+
     function process()
     {
         $this->Output = array();
-        
+
         switch($this->InputData->action)
         {
             case 'init':
@@ -83,19 +80,19 @@ class Burlesque
                 $this->Output['error'] = "Input Validation Error";
         }
     }
-    
+
     function getRooms()
     {
         $rooms_list = array();
         $rooms = $this->Database->get_room_list(
-                                    $this->Visitor->get('user_id'));
+                                    $this->XF->getUserId());
         foreach($rooms as $_room)
         {
             $rooms_list[] = Room::fromDBResult($_room)->toArray();
         }
         return $rooms_list;
     }
-    
+
     function getColors()
     {
         $color_list = array();
@@ -106,35 +103,35 @@ class Burlesque
         }
         return $color_list;
     }
-    
+
     function getRoom($room_id)
     {
         return Room::fromDBResult($this->Database->get_room($room_id));
     }
-    
+
     function doLogin()
     {
         //Get user login details (room, desired name, forum info, etc)
         $room = $this->getRoom($this->InputData->data->room_id);
         $display_name   = $this->InputData->data->display_name;
-        $forum_name     = $this->Visitor->get('username');
+        $forum_name     = $this->getUserName();
         if(!$room->allow_alias)
         {
             $display_name = $forum_name;
         }
         //Try to load user details from database
         $user = User::fromDBResult(
-                    $this->Database->get_user(  $display_name, 
+                    $this->Database->get_user(  $display_name,
                                                 $room->id));
         if(!$user->id)
         {
             //User not in DB.  Create new for current room
             $user = new User();
             $user->display_name     = $display_name;
-            $user->forum_id         = $this->Visitor->get('user_id');
+            $user->forum_id         = $this->XF->getUserId();
             $user->forum_name       = $forum_name;
             $user->room_id          = $room->id;
-            $user->id               = $this->Database->add_user($user, 
+            $user->id               = $this->Database->add_user($user,
                                                                 $room->id);
         }
         else
@@ -149,33 +146,33 @@ class Burlesque
             $user = User::fromDBResult(
                                 $this->Database->get_user_by_id($user->id));
         }
-        
+
         //Store user details in session
-        $this->Session->set('room'.$room->id.'user'.$user->id, array(
+        $this->XF->Session->set('room'.$room->id.'user'.$user->id, array(
                 'color'=> $this->InputData->data->color,
                 'font' => $this->InputData->data->font,
                 'name' => $user->display_name,
                 'id'   => $user->id
         ));
-        $this->Session->save();
-        
+        $this->XF->Session->save();
+
         //Login Post
         $this->doPost("Login", "#33xx33", false);
-        
+
         //Return User data to send to client
         return $user->toArray();
     }
-    
+
     function doPost($prefix = "", $prefix_color = "", $allow_commands = true)
     {
         $message = $this->InputData->data->message;
-        
+
         //Verify message has no illegal characters
         $message = htmlentities($message, ENT_QUOTES | ENT_IGNORE, "UTF-8");
-        
+
         $room = $this->getRoom($this->InputData->data->room_id);
         $display_name = $this->InputData->data->display_name;
-        $user = User::fromDBResult($this->Database->get_user(   $display_name, 
+        $user = User::fromDBResult($this->Database->get_user(   $display_name,
                                                                 $room->id));
         $post = new Post();
         $post->prefix           = $prefix;
@@ -189,22 +186,22 @@ class Burlesque
         $post->color            = $this->InputData->data->color;
         $post->font             = $this->InputData->data->room_id;
         $post->message          = $message;
-        
+
         if($allow_commands)
         {
             //Perform slashcommands on post
             $post = Burlesque_Commands($post);
         }
-        
+
         //Apply BBCode to message
         $message = Burlesque_BBCode($message);
-        
-        $post->id = $this->Database->add_post($post, $room->id, 
+
+        $post->id = $this->Database->add_post($post, $room->id,
 		                              $this->InputData->data->message);
-        
+
         return array("post"=>$post->toArray(), "user"=>$user->toArray());
-    }  
-        
+    }
+
     function getPosts()
     {
         $post_array = array();
@@ -213,7 +210,7 @@ class Burlesque
         {
             $post_array[] = Post::fromDBResult($_post_data)->toArray();
         }
-        
+
         return $post_array;
     }
 }
@@ -223,6 +220,6 @@ $input_data = json_decode(file_get_contents("php://input"));
 $Burlesque = new Burlesque($config, $input_data);
 $Burlesque->process();
 
-header('Content-Type','application/json; charset=UTF-8');  
+header('Content-Type','application/json; charset=UTF-8');
 echo json_encode($Burlesque->Output);
 ?>
