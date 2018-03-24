26edc4baff73dffa7e2c50c98553f56c3e0c49df
Kehvarl <kehvarl@gmail.com>
2018-03-24 11:33:14 -0700

    modified Role Seeder for clarity

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -15,19 +15,19 @@ class RoleTableSeeder extends Seeder
 
         $role_admin = New Role();
         $role_admin->name = 'admin';
-        $role_admin->description = 'An administrator';
+        $role_admin->description = 'an administrator';
         $role_admin->priority = 0;
         $role_admin->save();
 
         $role_verified = New Role();
         $role_verified->name = 'verified';
-        $role_verified->description = 'A verified user';
+        $role_verified->description = 'a verified user';
         $role_verified->priority = 90;
         $role_verified->save();
 
         $role_user = New Role();
         $role_user->name = 'user';
-        $role_user->description = 'Any user';
+        $role_user->description = 'a user';
         $role_user->priority = 100;
         $role_user->save();
     }
