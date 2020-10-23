99825ab8c8f64d04bc4662fecdfab33daed0cea9
kehvarl <kehvarl@kehvarl.com>
2020-10-23 16:47:54 -0700

    Rough in chat post model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    author: models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text: models.TextField()
+    created_date: models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
