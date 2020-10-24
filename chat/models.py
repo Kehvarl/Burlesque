cd9e57ecef7cb98445da790fd85465b1406ac658
kehvarl <kehvarl@kehvarl.com>
2020-10-23 17:13:38 -0700

    Initial post made.

--- chat/models.py
+++ chat/models.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    author: models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    text: models.TextField()
-    created_date: models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
