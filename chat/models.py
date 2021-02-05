9b4ac417461ce75e4b24ece66df3390fb8316412
kehvarl <kehvarl@kehvarl.com>
2021-02-04 19:25:52 -0700

    create Chat model

--- chat/models.py
+++ chat/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Chat(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=250)
-    roleplay = models.BooleanField()
-    masquerade = models.BooleanField()
+    roleplay = models.BooleanField(help_text="If Yes, this room is intended for Roleplaying")
+    masquerade = models.BooleanField(help_text="If Yes, then no user information will be present in chat, only the Character Name")
 
     def __str__ (self):
         return self.title
