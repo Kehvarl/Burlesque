14c2ccfe37c36db67f54d06714eb68253d2786a9
kehvarl <kehvarl@kehvarl.com>
2021-02-05 19:20:32 -0700

    add framework for REST API

--- burlesque/settings.py
+++ burlesque/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    'rest_framework',
+
+    
     # Local
     'chat.apps.ChatConfig',
 ]
