e65d1fce88ef0c930235e87a1ac9d14298525e08
kehvarl <kehvarl@kehvarl.com>
2021-02-14 16:53:37 -0700

    Create models and migrate them

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    content = models.TextField()
+    color = models.CharField(max_length=7)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From: {name} in {room} at: {created_at}"
