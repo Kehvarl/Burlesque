fcd9eab3937a1c30184e800f866c5b738e2de7a3
kehvarl <kehvarl@kehvarl.com>
2021-02-17 18:37:19 -0700

    views require authentication

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Chat, Post
 from .serializers import ChatSerializer, PostSerializer
 
 class ChatList(generics.ListAPIView):
+    permission_classes=(permissions.IsAuthenticated,)
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
 class ChatPosts(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(permissions.IsAuthenticated,)
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
