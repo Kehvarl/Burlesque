31cc2ebac4a5ab632d3f6add944fb8ffc786caeb
kehvarl <kehvarl@kehvarl.com>
2021-02-17 18:43:04 -0700

    use project level permissions

--- api/views.py
+++ api/views.py
@@ -4,11 +4,9 @@ from .models import Chat, Post
 from .serializers import ChatSerializer, PostSerializer
 
 class ChatList(generics.ListAPIView):
-    permission_classes=(permissions.IsAuthenticated,)
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
 class ChatPosts(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes=(permissions.IsAuthenticated,)
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
