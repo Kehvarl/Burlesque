4157798c4d53d266d383cb717fc4ff3495891af9
Kehvarl <kehvarl@kehvarl.com>
2023-10-14 18:42:50 -0700

    Create some basic models and set up admin site

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Room, Post, Name
+from .permissions import IsAuthorOrReadOnly
+from .serializers import RoomSerializer, PostSerializer, UserSerializer, NameSerializer
+
+
+class ChatList(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ChatPosts(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class NameList(generics.ListCreateAPIView):
+    queryset = Name.objects.all()
+    serializer_class = NameSerializer
+
+
+class NameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Name.objects.all()
+    serializer_class = NameSerializer
