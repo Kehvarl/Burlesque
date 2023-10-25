645ce2dd9b07ed1cf6a3fc647eeeeb6b097c2587
Kehvarl <kehvarl@kehvarl.com>
2023-10-24 18:50:49 -0700

    test case for robots

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from http import HTTPStatus
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+class RobotsTxtTests(SimpleTestCase):
+    def test_get(self):
+        response = self.client.get("/robots.txt")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response['content-type'] == "text/plain"
+
